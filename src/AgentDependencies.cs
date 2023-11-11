5df820b2780bc7aa39c02c3ea475a7d08f50d8d8
Sam Loeschen <samloeschen@gmail.com>
2023-11-11 17:39:38 -0500

    fix multiple deps

--- src/AgentDependencies.cs
+++ src/AgentDependencies.cs
@@ -1,29 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 public partial class AgentDependencies: RigidBody2D {
-	System.Collections.Generic.Dictionary<System.Type, Node> depsDict;
+
+	Dictionary<System.Type, List<Node>> depsDict;
 	public T GetNodeOrNull<T>() where T: Node {
-		if (depsDict.TryGetValue(typeof(T), out Node n)) {
-			return (T)n;
+		if (depsDict.TryGetValue(typeof(T), out var list)) {
+			return (T)list[0];
 		}
 		return null;
 	}
 	AgentBehaviours _agentBehaviours;
 	public override void _Ready() {
 		int nChildren = GetChildCount();
-		depsDict = new System.Collections.Generic.Dictionary<System.Type, Node>(nChildren) {
-			{ typeof(RigidBody2D), this },
-			{ typeof(AgentDependencies), this }
+		depsDict = new System.Collections.Generic.Dictionary<System.Type, List<Node>>(nChildren) {
+			{ typeof(RigidBody2D), new List<Node>() {this} },
+			{ typeof(AgentDependencies), new List<Node>() {this} }
 		};
+
+		void AddNode(Node n) {
+			var t = n.GetType();
+			if (depsDict.TryGetValue(t, out var list)) {
+				list.Add(n);
+			}
+			else {
+				depsDict.Add(t, new List<Node>(32) { n });
+			}
+		}
 
 		for (int i = 0; i < nChildren; i++) {
 			var child = GetChild(i);
-			depsDict.Add(child.GetType(), child);
-
+			AddNode(child);
 			if (child is AgentBehaviours behaviours) {
 				int nBehaviours = behaviours.GetChildCount();
 				for (int j = 0; j < nBehaviours; j++) {
 					var behaviour = behaviours.GetChild(j);
-					depsDict.Add(behaviour.GetType(), behaviour);
+					AddChild(behaviour);
 				}
 				_agentBehaviours = behaviours;
 			}
