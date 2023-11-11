e424b72a09475abcdb57a0b2d795671c7bc5f404
Sam Loeschen <samloeschen@gmail.com>
2023-11-11 11:16:50 -0500

    spawn some guys

--- src/CustomShaders.cs
+++ src/CustomShaders.cs
@@ -14,11 +14,11 @@ static class CustomShaders {
 
 
     const string UV_TEST_CODE = @"
-		shader_type canvas_item;
-		uniform sampler2D src : source_color, filter_nearest;
-		void fragment() {
-			COLOR.rgba = vec4(UV, 1.0, 1.0);
-		}
+      shader_type canvas_item;
+      uniform sampler2D src : source_color, filter_nearest;
+      void fragment() {
+        COLOR.rgba = vec4(UV, 1.0, 1.0);
+      }
     ";
     public static readonly Shader UV_TEST = new() {
         Code = UV_TEST_CODE,
---
+++ src/Spawner.cs
@@ -0,0 +1,25 @@
+using Godot;
+partial class Spawner: Node {
+
+    [Export] public int numSpawnPoints = 32;
+    [Export] public uint seed = 1;
+    [Export] public Node agentParent;
+
+    [Export] public PackedScene sceneToSpawn;
+    Vector2[] spawnPts;
+
+    public override void _Ready() {
+
+        // TODO bake these
+        spawnPts = new Vector2[numSpawnPoints];
+        BlueNoisePoints.GenerateBlueNoiseSamplePoints(ref spawnPts, seed);
+
+        foreach (var pt in spawnPts) {
+            var root = sceneToSpawn.Instantiate();
+            agentParent.AddChild(root);
+            if (root is Node2D node2D) {
+                node2D.Position = pt * GetViewport().GetVisibleRect().Size;
+            }
+        }
+    }
+}
