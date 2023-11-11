27fbca99a56dd1609624b4d09bcaa6fb4e849518
Sam Loeschen <samloeschen@gmail.com>
2023-11-11 15:01:34 -0500

    beep boop

--- src/RandomVelocityOnReady.cs
+++ src/RandomVelocityOnReady.cs
@@ -3,11 +3,17 @@ using System;
 
 public partial class RandomVelocityOnReady : Node, IBehaviourNode {
 
+	[Export] public float initialVelocityMin;
+	[Export] public float initialVelocityMax;
+
 	public void Init(AgentDependencies deps) {
 		if (deps.GetNodeOrNull<RigidBody2D>() is {} rb) {
 			// rb.Position = new Vector2(5f, 10f);
-			rb.SetAxisVelocity(Vector2.Right * 10f);
-			rb.LinearVelocity += Vector2.Right * 10f;
+			// rb.SetAxisVelocity(Vector2.Right * 10f);
+			rb.LinearVelocity += new Vector2 {
+				X = GD.Randf() - 0.5f,
+				Y = GD.Randf() - 0.5f,
+			}.Normalized() * (float)GD.RandRange(initialVelocityMin, initialVelocityMax);
 		}
 	}
 
