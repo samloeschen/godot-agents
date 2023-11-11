8b7fdea6dc6b56bfc21bbcb78b51add040c66aeb
Sam Loeschen <samloeschen@gmail.com>
2023-11-11 10:36:43 -0500

    get rid of old main code

--- src/Main.cs
+++ src/Main.cs
@@ -1,20 +1,7 @@
 using Godot;
 partial class Main: Node {
-    [Export] public SubViewport worldViewport;
-    [Export] public ColorRect displayRect;
-    [Export] public ColorRect backgroundRect;
 
     public override void _Ready() {
-        // set up viewport, backgrounds, etc
-        var displayMat = new ShaderMaterial {
-            Shader = CustomShaders.CANVAS_BLIT,
-        };
-        displayMat.SetShaderParameter("src", worldViewport.GetTexture());
-        displayRect.Material = displayMat;
 
-        var uvTestMat = new ShaderMaterial {
-            Shader = CustomShaders.UV_TEST,
-        };
-        backgroundRect.Material = uvTestMat;
     }
 }   
