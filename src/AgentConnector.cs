30457cc13bfad663f07f53341ff914d45e054e33
uvwar <uvwarr@gmail.com>
2023-11-12 13:45:31 -0500

    connector scene + fixes

--- src/AgentConnector.cs
+++ src/AgentConnector.cs
@@ -2,54 +2,54 @@ using Godot;
 using System;
 using System.Collections.Generic;
 public partial class AgentConnector: Area2D, IBehaviourNode {
-    [Export] public Godot.Collections.Array<int> validPolarities = new Godot.Collections.Array<int>();
-    [Export] public int maxConnections = 1;
-    [Export] public bool overrideAgentPolarity = false;
-    [Export] public int customPolarity = -1;
-    [Export] public bool alignRotation;
+	[Export] public Godot.Collections.Array<int> validPolarities = new Godot.Collections.Array<int>();
+	[Export] public int maxConnections = 1;
+	[Export] public bool overrideAgentPolarity = false;
+	[Export] public int customPolarity = -1;
+	[Export] public bool alignRotation;
 
-    List<AgentConnector> _activeConns;
-    AgentDependencies _deps;
-    int? _polarity;
-    public int? GetPolarity() => _polarity;
+	List<AgentConnector> _activeConns;
+	AgentDependencies _deps;
+	int? _polarity;
+	public int? GetPolarity() => _polarity;
 
-    public AgentDependencies GetAgent() => _deps;
+	public AgentDependencies GetAgent() => _deps;
 
-    public Vector2 GetVelocity() => _deps.LinearVelocity;
+	public Vector2 GetVelocity() => _deps.LinearVelocity;
 
-    // this passes the OTHER agent, not itself
-    public event Action<AgentDependencies> OnConnected;
+	// this passes the OTHER agent, not itself
+	public event Action<AgentDependencies> OnConnected;
 
-    public void Init(AgentDependencies deps) {
-        _deps = deps;
-        if (overrideAgentPolarity) {
-            _polarity = customPolarity;
-        }
-        else if (deps.GetNodeOrNull<AgentPolarity>() is {} ap) {
-            _polarity = ap.polarity;
-        } else {
-            _polarity = null;
-        }
-    }
+	public void Init(AgentDependencies deps) {
+		_deps = deps;
+		if (overrideAgentPolarity) {
+			_polarity = customPolarity;
+		}
+		else if (deps.GetNodeOrNull<AgentPolarity>() is {} ap) {
+			_polarity = ap.polarity;
+		} else {
+			_polarity = null;
+		}
+	}
 
-    public override void _Ready() {
-        _activeConns = new List<AgentConnector>(32);
-        AreaEntered += Entered;
-        // AreaExited += Exited;
-    }
+	public override void _Ready() {
+		_activeConns = new List<AgentConnector>(32);
+		AreaEntered += Entered;
+		// AreaExited += Exited;
+	}
 
-    void Entered(Area2D area) {
-        if (area is AgentConnector conn) {
-            if (_activeConns.Contains(conn)) return;
-            var other = conn.GetAgent();
-            if (other == _deps) return;
+	void Entered(Area2D area) {
+		if (area is AgentConnector conn) {
+			if (_activeConns.Contains(conn)) return;
+			var other = conn.GetAgent();
+			if (other == _deps) return;
 
-            if (conn._polarity is {} polarity) {
-                if (validPolarities.Contains(polarity)) {
-                    OnConnected?.Invoke(other);
-                    ConnectionManager.CreateConnection(this, conn);
-                }
-            }
-        }
-    }
+			if (conn._polarity is {} polarity) {
+				if (validPolarities.Contains(polarity)) {
+					OnConnected?.Invoke(other);
+					ConnectionManager.CreateConnection(this, conn);
+				}
+			}
+		}
+	}
 }
--- src/Blinker.cs
+++ src/Blinker.cs
@@ -3,44 +3,44 @@ using System;
 
 public partial class Blinker : Node, IBehaviourNode
 {
-    [Export(PropertyHint.Range, "0,2")] public double waitTime;
+	[Export(PropertyHint.Range, "0,2")] public double waitTime;
 
-    TextureRect eye;
-    TextureRect pupil;
-    Timer timer;
+	TextureRect eye;
+	TextureRect pupil;
+	Timer timer;
 
-    public override void _PhysicsProcess(double delta)
-    {
-        if (GD.Randf() < 0.005f) Blink();
-        if (!this.timer.IsStopped())
-        {
-            var scale = this.pupil.Scale;
-            var timePercentage = this.timer.TimeLeft / this.timer.WaitTime;
+	public override void _PhysicsProcess(double delta)
+	{
+		if (GD.Randf() < 0.005f) Blink();
+		if (!this.timer.IsStopped())
+		{
+			var scale = this.pupil.Scale;
+			var timePercentage = this.timer.TimeLeft / this.timer.WaitTime;
 
-            var newY = (float)((Math.Cos(timePercentage * Math.PI * 2) + 1) / 2);
+			var newY = (float)((Math.Cos(timePercentage * Math.PI * 2) + 1) / 2);
 
-            this.eye.Scale = new Vector2(this.eye.Scale.X, newY);
-            this.pupil.Scale = new Vector2(this.pupil.Scale.X, newY);
-        }
-    }
+			this.eye.Scale = new Vector2(this.eye.Scale.X, newY);
+			this.pupil.Scale = new Vector2(this.pupil.Scale.X, newY);
+		}
+	}
 
-    public void Blink()
-    {
-        if (this.timer.IsStopped()) this.timer.Start();
-    }
+	public void Blink()
+	{
+		if (this.timer.IsStopped()) this.timer.Start();
+	}
 
-    public void StopBlinking() {
-        this.timer.Stop();
-    }
+	public void StopBlinking() {
+		this.timer.Stop();
+	}
 
-    public void Init(AgentDependencies deps)
-    {
-        if (deps.GetNodeOrNull<RigidBody2D>() is { } rb)
-        {
-            this.pupil = rb.GetNode<TextureRect>("Pupil");
-            this.eye = rb.GetNode<TextureRect>("Eye");
-            this.timer = (Timer)rb.FindChild("BlinkTimer");
-            this.timer.Timeout += StopBlinking;
-        }
-    }
+	public void Init(AgentDependencies deps)
+	{
+		if (deps.GetNodeOrNull<RigidBody2D>() is { } rb)
+		{
+			this.pupil = rb.GetNode<TextureRect>("Pupil");
+			this.eye = rb.GetNode<TextureRect>("Eye");
+			this.timer = (Timer)rb.FindChild("BlinkTimer");
+			//this.timer.Timeout += StopBlinking;
+		}
+	}
 }
